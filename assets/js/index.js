22ae829078dfa02e53503661de83c03e1de7537d
Cosme_Miralles <cosme.upf@gmail.com>
2023-08-02 20:46:14 +0200

    vale, a parte de la alineación también he corregido la validación de fecha y ha parametrizado los límites en en el archivo JS para que los tome de HTML porque estaban hardcodeados.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-    console.log("Hello World, the DOM is loaded.");
+    //console.log("El DOM ha sido cargado correctamente.");
 
     const destinoInput = document.getElementById("FormSelectDestino");
     const presupuestoInput = document.getElementById("presupuesto-input");
@@ -9,8 +9,8 @@ window.addEventListener('DOMContentLoaded', () => {
     const sorteoButton = document.querySelector("#draw-lottery-ticket-button");
     const lotteryBox = document.querySelector(".lottery-container");
 
-    console.log(destinoInput.id, presupuestoInput.id,fechaSalidaInput.id);
-    console.log(sorteoButton);
+    //console.log(destinoInput.id, presupuestoInput.id,fechaSalidaInput.id);
+    //console.log(sorteoButton);
 
     sorteoButton.addEventListener("click", (event) => {
         event.preventDefault();
@@ -18,80 +18,74 @@ window.addEventListener('DOMContentLoaded', () => {
         const destino = destinoInput.value;
         const presupuesto = presupuestoInput.value;
         const fechaSalida = fechaSalidaInput.value;
+
         let fechaSalidaFormat = new Date(fechaSalida)
         //añadido para compensar la diferencia horaria
         fechaSalidaFormat.setMinutes(fechaSalidaFormat.getMinutes() + fechaSalidaFormat.getTimezoneOffset())
-        const fechaFormateada = fechaSalidaFormat.toLocaleDateString();
+        // formateo de la fecha de salida
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        const fechaFormateada = fechaSalidaFormat.toLocaleDateString('es-ES', options);
+        //console.log(fechaFormateada);
 
-        const  valueSelected = destinoInput.options[destinoInput.selectedIndex].text;
-  
-     
-
+        const valueSelected = destinoInput.options[destinoInput.selectedIndex].text;
+        
         if (destino === "") {
             destinoInput.classList.add("is-invalid");
-
-          } else {
+        } else {
             destinoInput.classList.remove("is-invalid");
- 
-          }
-          
-          
-        if(presupuesto < 100){
+        }
+        
+        if (presupuesto < presupuestoInput.min || presupuesto > presupuestoInput.max) {
             presupuestoInput.classList.add("is-invalid");
-        } else{
+        } else {
             presupuestoInput.classList.remove("is-invalid");
-
         }
-        if(fechaSalida ===""){
+        
+        if (fechaSalida < fechaSalidaInput.min || fechaSalida > fechaSalidaInput.max) {
             fechaSalidaInput.classList.add("is-invalid");
-        } else{
+        } else {
             fechaSalidaInput.classList.remove("is-invalid");
-            
         }
-
-        if(destino !== "" && presupuesto > 99 && presupuesto < 4001 && fechaSalida !== ""){
-
-                lotteryBox.style.backgroundImage = `url(./assets/img/${destino}.jpg)`;
-            
-                lotteryBox.innerHTML =
-                ` <div class="title">
-                
-                <div class="info">
-                    <h4 id="tituloDestino" class="name">Felicidades!!<br> has ganado un viaje a </br> ${valueSelected}</h4>
-                </div>
+        
+        if (destino !== "" && presupuesto >= presupuestoInput.min && presupuesto <= presupuestoInput.max && fechaSalida >= fechaSalidaInput.min && fechaSalida <= fechaSalidaInput.max) {
+            lotteryBox.style.backgroundImage = `url(./assets/img/${destino}.jpg)`;
+        
+            lotteryBox.innerHTML = `
+                <div class="title">
+                    <div class="info">
+                        <h4 id="tituloDestino" class="name">Felicidades!!<br> has ganado un viaje a </br> ${valueSelected}</h4>
+                    </div>
                 </div>
                 <div class="presupuesto">
-                <p id="tweet-description">Tu presupuesto aprobado </br> para gastos es: </br>${presupuesto}€</p>
+                    <p id="tweet-description">Tu presupuesto aprobado </br> para gastos es: </br>${presupuesto}€</p>
                 </div>
                 <div class="fechaSalida">
-                <p id="tweet-description">La fecha de </br> tu viaje es: </br>${fechaFormateada}</p>
+                    <p id="tweet-description">La fecha de </br> tu viaje es: </br>${fechaFormateada}</p>
                 </div>
             
                 <div class="bottom-section d-flex justify-content-end">
-                <div id="like-button" class="btn">Like <i class="fa fa-heart"></i></div>
+                    <div id="like-button" class="btn">Like <i class="fa fa-heart"></i></div>
                 </div>
-            `; 
+            `;
+        
+            console.log(destino, presupuesto, fechaFormateada);
+
+            destinoInput.value = "";
+            presupuestoInput.value = 0;
+            fechaSalidaInput.value = "";
+        
+            sorteoButton.classList.add("btn-success");
+        
+            setTimeout(() => {
+                sorteoButton.classList.remove("btn-success");
+            }, 1500);
             
-                console.log(destino, presupuesto, fechaFormateada);
-
-                destinoInput.value = "";
-                presupuestoInput.value = 0;
-                fechaSalidaInput.value="";
-          
-                sorteoButton.classList.add("btn-success");
-          
-                setTimeout(() => {
-                    sorteoButton.classList.remove("btn-success");
-                }, 1500);
-              } else {
-                sorteoButton.classList.add("btn-danger");
-          
-                setTimeout(() => {
-                    sorteoButton.classList.remove("btn-danger");
-                }, 1500);
-              }
-
-            });
-
-
+        } else {
+            sorteoButton.classList.add("btn-danger");
+        
+            setTimeout(() => {
+                sorteoButton.classList.remove("btn-danger");
+            }, 1500);
+        }
+    });
 });
